d08691299e78ba88bf8f1f713309fcd50bc4f898
Zeuxis Lo <seekstudio@gmail.com>
2012-12-29 21:28:11 +0800

    added to hexo convert script

--- convert-to-hexo.py
+++ convert-to-hexo.py
@@ -6,7 +6,9 @@ reload(sys)
 
 sys.setdefaultencoding("utf-8")
 
+import unicodedata
 import re
+import urllib2
 from datetime import datetime
 
 try:
@@ -18,6 +20,23 @@ feedparser.SANITIZE_HTML = 0
 #feedparser._HTMLSanitizer.acceptable_elements.append('coolcode')
 
 class Parser(object):
+	URL = 'http://translate.google.com.hk/translate_a/t?client=t&hl=zh-TW&sl=zh-CN&tl=en&ie=UTF-8&oe=UTF-8&multires=1&otf=2&ssel=3&tsel=3&sc=1'
+	HEADER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1'
+
+	def translate(self, input):
+		search_url = self.URL + '&text=' + urllib2.quote(input.encode("UTF8"))
+		req = urllib2.Request(search_url)
+		req.add_header('User-Agent', self.HEADER_AGENT)
+		r = urllib2.urlopen(req)
+		value = r.read().split('"')[1]
+		return value
+		
+	def seo_string(self, x):
+		r = unicodedata.normalize('NFKD',x).encode('cp1256','ignore')
+		r = unicode(re.sub('[^\w\s-]','',r).strip().lower())
+		r = re.sub('[-\s]+','-',r)
+		return r
+
 	def parse(self):
 		feed = feedparser.parse("export.xml")
 
@@ -27,10 +46,13 @@ class Parser(object):
 			if entry.wp_status == "publish" and entry.wp_post_type == "post":
 				wordpress_id = entry.wp_post_id
 				date = datetime.strptime(entry.wp_post_date, "%Y-%m-%d %H:%M:%S")
-				name = "%02d-%02d-%02d-%s.markdown" % (date.year, date.month, date.day, wordpress_id)
-
 				date_string = "%02d-%02d-%02d" % (date.year, date.month, date.day)
 				title_string = entry.title.replace("'", '&#39;')
+				
+				title = re.sub(r'-(.*)-', r'\1', self.seo_string(self.translate(title_string).replace(" ", "-").decode("UTF8")))
+				name = "%s.md" % (title)
+				
+				#name = "%02d-%02d-%02d-%s.markdown" % (date.year, date.month, date.day, wordpress_id)
 
 				content = entry.content[0].value
 				content = re.sub(r'^<pre[^>]*>$', '{% codeblock %}' + "\n", content)
@@ -49,9 +71,9 @@ class Parser(object):
 				content = content.replace('< ?php', '<?php')
 
 				to_english_tags = {
-					u'伺服器'		: 'Server',
+					u'伺服器'	: 'Server',
 					u'忽然一感'	: 'Life',
-					u'未分類'		: 'Other',
+					u'未分類'	: 'Other',
 					u'案子'		: 'Case',
 					u'程式作品'	: 'Project',
 					u'程式修改'	: 'Code Modifiy',
@@ -61,7 +83,7 @@ class Parser(object):
 					u'網絡見聞'	: 'Web Link',
 					u'網路代碼'	: 'Web Coding',
 					u'網路文章'	: 'Web Article',
-					u'編輯器'		: 'Editor',
+					u'編輯器'	: 'Editor',
 					u'資料收集'	: 'Research',
 					u'軟體應用'	: 'Software'
 				}
@@ -75,7 +97,7 @@ class Parser(object):
 						else:
 							tags.append("- " +  key.encode('utf8'))
 
-				print("Converting: %s" % name)
+				print("Converting: %s" % (name))
 
 				f = open("_posts/%s" % name, 'w+')
 				f.write("---\n")
@@ -83,7 +105,7 @@ class Parser(object):
 				f.write("title: '%s'\n" % title_string.encode("utf8"))
 				f.write("date: %s\n" % date_string)
 				f.write("wordpress_id: %s\n" % wordpress_id)
-				f.write("permalink: /blog/archives/%s\n" % wordpress_id)
+				#f.write("permalink: /blog/archives/%s\n" % wordpress_id)
 				f.write("comments: true\n")
 				f.write("categories:\n")
 				f.write("%s\n" % "\n".join(tags))
--- run.sh
+++ run.sh
@@ -1,10 +1,26 @@
-echo -e "\033[;35;5mConverting..."
-python convert.py
+#!/bin/bash
 
-echo -e "\033[;35;5mRemoving..."
-rm -Rf octopress/source/_posts/*
+stop=0
 
-echo -e "\033[;35;5mCopying..."
-cp -Rf _posts/* octopress/source/_posts/
+select convertor in octopress hexo quit; do
+	if [ $convertor = "octopress" ]; then
+		echo -e "\033[;35;5mConverting..."
+		python convert-to-octopress.py
+	elif [ $convertor = "hexo" ]; then
+		echo -e "\033[;35;5mConverting..."
+		python convert-to-hexo.py
+	elif [ $convertor = "quit" ]; then
+		stop=1
+	fi
+	break
+done
 
-echo -e "\033[;32;5mDone"
+if [ $stop = 0 ]; then
+	echo -e "\033[;35;5mRemoving..."
+	rm -Rf octopress/source/_posts/*
+	
+	echo -e "\033[;35;5mCopying..."
+	cp -Rf _posts/* octopress/source/_posts/
+	
+	echo -e "\033[;32;5mDone"
+fi
